18c41efde762efa57944d3898ec5e42442fba99c
Malikahan <rmalikahan@gmail.com>
2022-12-07 18:29:50 +0600

    dcc

--- src/pages/allList.vue
+++ src/pages/allList.vue
@@ -2,9 +2,6 @@
 <script setup lang="ts">
 import Nav from "../layouts/component/nav.vue";
 </script>
-<style src="../assets/css/reset.css"></style>
-<style src="../assets/css/style.css"></style>
-<style src="../assets/css/allList.css"></style>
 <style scoped>
 nav {
   margin-top: 70px;
