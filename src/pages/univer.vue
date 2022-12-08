cf97ca9a1e67c59244ca06514174c4f178d623aa
Bayelwp1 <bayelwp1@gmail.com>
2022-12-08 16:47:02 +0300

    Bayel finished sections

--- src/pages/univer.vue
+++ src/pages/univer.vue
@@ -2,11 +2,11 @@
   <header class="header"></header>
 
   <section class="faculty">
-    <div class="container">
+    <div class="my__container">
       <h1 class="title">Faculty</h1>
       <div class="flex-row">
         <h3>OshMU</h3>
-        <div class="row">
+        <div class="my__row">
           <div class="col">
             <div class="block">
               <div class="img__and__text">
@@ -140,7 +140,7 @@
     </div>
   </section>
   <section class="documents">
-    <div class="container">
+    <div class="my__container">
       <h1 class="title">Documents</h1>
       <div class="long__blocks">
         <div class="long__block">
@@ -171,9 +171,101 @@
     </div>
   </section>
   <section class="price">
-    <div class="container">
+    <div class="my__container">
       <h1 class="title">Price</h1>
-      <div class="price__border"></div>
+      <div class="price__border">
+        <div class="price__row">
+          <div class="price__col">
+            <div class="price__block">
+              <h3 class="price__btitle">For Start</h3>
+              <p class="price__text">1200$</p>
+            </div>
+          </div>
+          <div class="price__col">
+            <div class="price__block">
+              <h3 class="price__btitle">Study</h3>
+              <p class="price__text">6200$</p>
+            </div>
+          </div>
+          <div class="price__col">
+            <div class="price__block">
+              <h3 class="price__btitle">TAX</h3>
+              <p class="price__text">800$</p>
+            </div>
+          </div>
+          <div class="price__col">
+            <div class="total__price">
+              <h3 class="price__btitle">Total</h3>
+              <p class="price__text">8200$</p>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </section>
+  <section class="others">
+    <div class="my__container">
+      <h1 class="title">Others</h1>
+      <div class="others__row">
+        <div class="others__col">
+          <div class="others__block">
+            <div>
+              <h3 class="others__title">Nutrition</h3>
+              <p class="others__description">
+                Christian Bale (born 1974) is an English actor. Known for his
+                versatility and recurring physical
+              </p>
+            </div>
+            <div class="others__block__and__link">
+              <div class="others__buttons">
+                <button class="btn btn-dark">Only personal</button>
+              </div>
+              <a class="link" href="#"
+                >More <img src="../assets/img/next.png" alt=""
+              /></a>
+            </div>
+          </div>
+        </div>
+        <div class="others__col">
+          <div class="others__block">
+            <div>
+              <h3 class="others__title">Residence</h3>
+              <p class="others__description">
+                Christian Bale (born 1974) is an English actor. Known for his
+                versatility and recurring physical
+              </p>
+            </div>
+            <div class="others__block__and__link">
+              <div class="others__buttons">
+                <button class="btn btn-dark">Personal</button>
+                <button class="btn btn-dark">In university</button>
+              </div>
+              <router-link to="/residence" class="link" href="#"
+                >More<img src="../assets/img/next.png" alt=""
+              /></router-link>
+            </div>
+          </div>
+        </div>
+        <div class="others__col">
+          <div class="others__block">
+            <div>
+              <h3 class="others__title">Air tickets</h3>
+              <p class="others__description">
+                Christian Bale (born 1974) is an English actor. Known for his
+                versatility and recurring physical
+              </p>
+            </div>
+            <div class="others__block__and__link">
+              <div class="others__buttons">
+                <button class="btn btn-dark">Only personal</button>
+              </div>
+              <a class="link" href="#"
+                >More <img src="../assets/img/next.png" alt=""
+              /></a>
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
   </section>
 </template>
@@ -188,7 +280,7 @@ body {
   background-size: cover;
   height: 100vh;
 }
-.container {
+.my__container {
   width: 1140px;
   margin: auto;
 }
@@ -215,8 +307,9 @@ body {
   line-height: 147px;
   letter-spacing: -0.01em;
   color: #323130;
+  margin-top: 100px;
 }
-.row {
+.my__row {
   display: flex;
   flex-flow: wrap;
   margin-left: 20px;
@@ -275,7 +368,7 @@ body {
   display: flex;
   align-items: center;
   justify-content: space-between;
-  padding: 15px 45px;
+  padding: 40px 45px;
   margin-bottom: 35px;
 }
 .long__block__title {
@@ -307,10 +400,109 @@ body {
   color: #323130;
 }
 
-.price__border {
-  width: 1140px;
+.price__row {
+  width: 1240px;
   height: 343px;
-  background-color: black;
+  border: 3px solid #323130;
   border-radius: 33px;
+  display: flex;
+  flex-flow: wrap;
+  align-items: center;
+  margin-left: -50px;
+}
+.price__col {
+  width: 25%;
+  padding-left: 50px;
+}
+.price__block {
+  width: 250px;
+  height: 250px;
+  background: #d9d9d9;
+  border-radius: 22px;
+  display: flex;
+  flex-direction: column;
+  align-items: flex-start;
+  justify-content: space-between;
+  padding: 15px;
+}
+.price__btitle {
+  font-family: "Gilroy";
+  font-style: normal;
+  font-weight: 700;
+  font-size: 40px;
+  line-height: 50px;
+  letter-spacing: -0.01em;
+  color: #323130;
+}
+.price__text {
+  font-family: "Gilroy";
+  font-style: normal;
+  font-weight: 500;
+  font-size: 40px;
+  line-height: 49px;
+  letter-spacing: -0.01em;
+  color: #323130;
+}
+
+.others__row {
+  display: flex;
+  flex-flow: wrap;
+  align-items: center;
+  margin-left: -80px;
+}
+.others__col {
+  width: 50%;
+  padding-left: 80px;
+  padding-bottom: 80px;
+}
+.others__block {
+  width: 540px;
+  height: 540px;
+  background: #d9d9d9;
+  border-radius: 22px;
+  display: flex;
+  flex-direction: column;
+  align-items: flex-start;
+  justify-content: space-between;
+  padding: 40px;
+}
+.others__title {
+  font-family: "Gilroy";
+  font-style: normal;
+  font-weight: 700;
+  font-size: 56px;
+  line-height: 69px;
+  letter-spacing: -0.01em;
+  color: #323130;
+}
+.others__description {
+  font-family: "Gilroy";
+  font-style: normal;
+  font-weight: 600;
+  font-size: 17px;
+  line-height: 25px;
+  letter-spacing: -0.01em;
+  color: #323130;
+}
+.others__block__and__link {
+  width: 100%;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+}
+.others__block__and__link button {
+  width: 220px;
+  height: 60px;
+  border-radius: 12px;
+  margin-bottom: 20px;
+}
+.others__block__and__link .link {
+  font-family: "Gilroy";
+  font-style: normal;
+  font-weight: 600;
+  font-size: 17px;
+  line-height: 25px;
+  letter-spacing: -0.01em;
+  color: #323130;
 }
 </style>
