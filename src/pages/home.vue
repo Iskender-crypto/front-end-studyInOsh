a47b1eabe0146b20c0bf006e6f2df6e11d5168c0
Muhammadfarruk <muhammadfarrukmamirzhanov@gmail.com>
2022-12-15 16:50:31 +0600

    banner

--- src/pages/home.vue
+++ src/pages/home.vue
@@ -1,4 +1,13 @@
 <template>
-  <h1>hhhhh</h1>
+  <header class="header_banner"></header>
 </template>
 <script setup lang="ts"></script>
+<style scoped>
+.header_banner {
+  background-image: url("../assets/img/Header.png");
+  background-position: center;
+  background-size: cover;
+  background-repeat: no-repeat;
+  height: 930px;
+}
+</style>
