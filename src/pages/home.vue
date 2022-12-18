0cad4ada04d07239f5898a2f4290940d2d27c9db
Muhammadfarruk <muhammadfarrukmamirzhanov@gmail.com>
2022-12-18 19:37:05 +0600

    two section were ended

--- src/pages/home.vue
+++ src/pages/home.vue
@@ -1,4 +1,6 @@
 <template>
+  <!-- ======================== Header =============================== -->
+
   <header class="header_banner">
     <div class="header container">
       <!-- ======================== Header Nav =============================== -->
@@ -107,11 +109,140 @@
       </div>
     </div>
   </header>
+
+  <!-- ======================== About =============================== -->
+
+  <section class="About">
+    <div class="container">
+      <div class="about__container">
+        <div class="about__left">
+          <p class="about__h6">About us</p>
+
+          <p class="about__p">
+            Christian Bale (born 1974) is an English actor. Known for his
+            versatility and recurring physical
+          </p>
+        </div>
+
+        <div class="about__right">
+          <div class="about__h">
+            <h3 class="about__h3">72</h3>
+            <p class="about__p">
+              Christian Bale (born 1974) is an English actor. Known for his
+              versatility and recurring physical
+            </p>
+          </div>
+
+          <div class="about__h">
+            <h3 class="about__h3">11</h3>
+            <p class="about__p2">
+              Christian Bale (born 1974) is an English actor. Known for his
+              versatility and recurring physical
+            </p>
+          </div>
+
+          <div class="about__h">
+            <h3 class="about__h3">9</h3>
+            <p class="about__p">
+              Christian Bale (born 1974) is an English actor. Known for his
+              versatility and recurring physical
+            </p>
+          </div>
+        </div>
+      </div>
+    </div>
+  </section>
+
+  <!-- ======================== Advantage =============================== -->
+
+  <section class="Advantage">
+    <div class="container">
+      <div class="advantage__container">
+        <h1 class="advantage__h1">Preimushestva</h1>
+
+        <p class="advantage__p">
+          Christian Bale (born 1974) is an English actor. Known for his
+          versatility and recurring physical
+        </p>
+
+        <div class="advantage__row">
+          <div class="advantage__col">
+            <div class="advantage__block">
+              <div class="advantage__inblock">
+                <img
+                  class="advantage__img"
+                  src="../assets/img/benefits1.png"
+                  alt=""
+                />
+
+                <p class="advantage__p2">
+                  Christian Bale (born 1974) is an English actor. Known for his
+                  versatility and recurring physical
+                </p>
+              </div>
+            </div>
+          </div>
+          <div class="advantage__col">
+            <div class="advantage__block">
+              <div class="advantage__inblock">
+                <img
+                  class="advantage__img2"
+                  src="../assets/img/benefits2.png"
+                  alt=""
+                />
+
+                <p class="advantage__p2">
+                  Christian Bale (born 1974) is an English actor. Known for his
+                  versatility and recurring physical
+                </p>
+              </div>
+            </div>
+          </div>
+          <div class="advantage__col">
+            <div class="advantage__block">
+              <div class="advantage__inblock">
+                <img
+                  class="advantage__img3"
+                  src="../assets/img/benefits3.png"
+                  alt=""
+                />
+
+                <p class="advantage__p2">
+                  Christian Bale (born 1974) is an English actor. Known for his
+                  versatility and recurring physical
+                </p>
+              </div>
+            </div>
+          </div>
+          <div class="advantage__col">
+            <div class="advantage__block">
+              <div class="advantage__inblock">
+                <img
+                  class="advantage__img4"
+                  src="../assets/img/benefits4.png"
+                  alt=""
+                />
+
+                <p class="advantage__p2">
+                  Christian Bale (born 1974) is an English actor. Known for his
+                  versatility and recurring physical
+                </p>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </section>
 </template>
+
 <script setup lang="ts">
 import Nav from "../layouts/component/nav.vue";
 </script>
+
 <style scoped>
+/* ========================== Header ================================== */
+
 .header_banner {
   background-image: url("../assets/img/Header.png");
   background-position: center;
@@ -280,5 +411,176 @@ import Nav from "../layouts/component/nav.vue";
   line-height: 17px;
 
   color: #ffffff;
+}
+
+/* ========================== About ================================== */
+.about__container {
+  width: 1053px;
+  height: 498px;
+  margin-top: 110px;
+  display: flex;
+  justify-content: space-between;
+}
+
+.about__left {
+  margin-top: 30px;
+}
+
+.about__h6 {
+  font-family: "Gilroy";
+  font-style: normal;
+  font-weight: 600;
+  font-size: 28px;
+  line-height: 34px;
+  letter-spacing: -0.01em;
+
+  color: #323130;
+}
+
+.about__p {
+  width: 354px;
+  height: 50px;
+  margin-top: 15px;
+
+  font-family: "Gilroy";
+  font-style: normal;
+  font-weight: 600;
+  font-size: 17px;
+  line-height: 25px;
+
+  letter-spacing: -0.01em;
+
+  color: #323130;
+}
+
+.about__right {
+  display: flex;
+  flex-direction: column;
+  justify-content: space-between;
+}
+
+.about__h3 {
+  font-family: "Gilroy";
+  font-style: normal;
+  font-weight: 600;
+  font-size: 56px;
+  line-height: 69px;
+
+  letter-spacing: -0.01em;
+
+  color: #323130;
+}
+.about__p2 {
+  width: 338px;
+  height: 54px;
+
+  font-family: "Gilroy";
+  font-style: normal;
+  font-weight: 600;
+  font-size: 17px;
+  line-height: 27px;
+
+  letter-spacing: -0.03em;
+
+  color: #838383;
+}
+
+/* ========================== Advantage ================================== */
+
+.advantage__container {
+  width: 1100px;
+  height: 1000px;
+}
+
+.advantage__h1 {
+  font-family: "Gilroy";
+  font-style: normal;
+  font-weight: 600;
+  font-size: 120px;
+  line-height: 147px;
+  letter-spacing: -0.01em;
+  margin-top: 90px;
+
+  color: #323130;
+}
+
+.advantage__p {
+  width: 354px;
+  height: 52px;
+
+  font-family: "Gilroy";
+  font-style: normal;
+  font-weight: 600;
+  font-size: 17px;
+  line-height: 25px;
+
+  margin-bottom: 38px;
+
+  letter-spacing: -0.01em;
+
+  color: #323130;
+}
+
+.advantage__row {
+  display: flex;
+  flex-flow: wrap;
+  margin-left: -40px;
+}
+
+.advantage__col {
+  width: 33.3333%;
+  padding-left: 40px;
+  margin-bottom: 45px;
+}
+
+.advantage__block {
+  width: 340px;
+  height: 350px;
+  background-color: whitesmoke;
+  border-radius: 15px;
+  display: flex;
+  flex-direction: column;
+  justify-content: space-between;
+  padding: 20px 0px 34px 39px;
+}
+
+.advantage__inblock {
+  height: 250px;
+  width: 210px;
+  display: flex;
+  flex-direction: column;
+  justify-content: space-between;
+}
+
+.advantage__img {
+  width: 62.86px;
+  height: 60.46px;
+}
+.advantage__img2 {
+  width: 51.43px;
+  height: 60.7px;
+}
+.advantage__img3 {
+  width: 57.14px;
+  height: 60.46px;
+}
+.advantage__img4 {
+  width: 51.43px;
+  height: 60.46px;
+}
+
+.advantage__p2 {
+  width: 210px;
+  height: 50px;
+
+  font-family: "Gilroy";
+  font-style: normal;
+  font-weight: 600;
+  font-size: 17px;
+  line-height: 25px;
+
+  letter-spacing: -0.01em;
+
+  color: #323130;
 }
 </style>
